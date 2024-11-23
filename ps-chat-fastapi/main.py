dee1de9fcfa2bcaa670fdc7fb28614b1c16a4a63
disk0dancer <gachurakov@edu.hse.ru>
2024-11-23 13:36:06 +0500

    init(api): upd commands

--- ps-chat-fastapi/main.py
+++ ps-chat-fastapi/main.py
@@ -2,11 +2,14 @@ from FastAPI import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 # start
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
