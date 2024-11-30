44066ef54401798d170d27197025c8c72761a194
disk0dancer <gachurakov@edu.hse.ru>
2024-11-30 12:33:03 +0500

    fix(ui): correct ui

--- ps-chat-vite-react/src/App.tsx
+++ ps-chat-vite-react/src/App.tsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// @ts-ignore
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    // @ts-ignore
+    const [clientId, setClienId] = useState(
+        Math.floor(new Date().getTime() / 1000)
+    );
+    // @ts-ignore
+    const [chatHistory, setChatHistory] = useState([]);
+    // @ts-ignore
+    const [isOnline, setIsOnline] = useState(false);
+    // @ts-ignore
+    const [textValue, setTextValue] = useState("");
+    const [websckt, setWebsckt] = useState();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [message, setMessage] = useState([]);
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const url = "ws://localhost:8000/ws/" + clientId;
+        const ws = new WebSocket(url);
+        // @ts-ignore
+        ws.onopen = (event) => {
+            ws.send("Connect");
+        };
+
+        // recieve message every start page
+        ws.onmessage = (e) => {
+            const message = JSON.parse(e.data);
+            setMessages((messages) => ([...messages, message]));
+        };
+        // @ts-ignore
+        setWebsckt(ws);
+        //clean up function when we close page
+        return () => ws.close();
+    }, []);
+
+    const sendMessage = () => {
+        // @ts-ignore
+        websckt.send(message);
+        // recieve message every send message
+        // @ts-ignore
+        websckt.onmessage = (e) => {
+            const message = JSON.parse(e.data);
+            setMessages([...messages, message]);
+        };
+        setMessage([]);
+    };
+
+    return (
+        <div className="container">
+            <h1>Chat</h1>
+            <h2>Your client id: {clientId} </h2>
+            <div className="chat-container">
+                <div className="chat">
+                    {messages.map((value, index) => {
+                        if (value.clientId === clientId) {
+                            return (
+                                <div key={index} className="my-message-container">
+                                    <div className="my-message">
+                                        <p className="client">client id : {clientId}</p>
+                                        <p className="message">{value.message}</p>
+                                    </div>
+                                </div>
+                            );
+                        } else {
+                            return (
+                                <div key={index} className="another-message-container">
+                                    <div className="another-message">
+                                        <p className="client">client id : {clientId}</p>
+                                        <p className="message">{value.message}</p>
+                                    </div>
+                                </div>
+                            );
+                        }
+                    })}
+                </div>
+                <div className="input-chat-container">
+                    <input
+                        className="input-chat"
+                        type="text"
+                        placeholder="Chat message ..."
+                        // @ts-ignore
+                        onChange={(e) => setMessage(e.target.value)}
+                        value={message}
+                    ></input>
+                    <button className="submit-chat" onClick={sendMessage}>
+                        Send
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
