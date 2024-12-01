1c9a2b18e8fe69c338eb604a0cbd411267ab1410
disk0dancer <gachurakov@edu.hse.ru>
2024-12-01 18:51:42 +0500

    feat(api): add api login and msg send

--- ps-chat-fastapi/app.py
+++ ps-chat-fastapi/app.py
@@ -1,14 +1,64 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from connection import ConnectionManager
+from typing import Optional, Dict, List
+from pydantic import BaseModel, Field
+
+
+class User(BaseModel):
+    login: str
+
+
+class Message(BaseModel):
+    message: str
+    user: User
+    message_id: str
+    time: Optional[str] = ""
+
+
+class ApplicationUsers(BaseModel):
+    users: Dict[str, User] = Field(default_factory=dict)
+
+    def add_user(self, user: User) -> bool:
+        if user.login not in self.users:
+            self.users[user.login] = user
+            return True
+        return False
+
+    def get_users(self) -> Dict[str, User]:
+        return self.users
+
+    def remove_user(self, user: User) -> bool:
+        # TODO: remove user if he  not fetched msgs for a long time
+        if user.login in self.users:
+            self.users.remove(user.login)
+            return True
+        return False
+
+
+class ChatHistory(BaseModel):
+    messages: Dict[str, Message] = Field(default_factory=dict)
+
+    def add_message(self, message: Message) -> bool:
+        # assert len(message.message_id) > 0, "Message ID must be set"
+        if len(message.message_id) > 0:
+            self.messages[message.message_id] = message
+            return True
+        return False
+
+    def get_messages(self) -> List[Message]:
+        return list(self.messages.values())
+
+    def remove_message(self, message: Message) -> bool:
+        if message.message_id in self.messages:
+            self.messages.remove(message.message_id)
+            return True
+        return False
+
 
 class Application:
     def __init__(self):
         self.api = FastAPI()
-        
-        self.manager = ConnectionManager()
-
         self.api.add_middleware(
             CORSMiddleware,
             allow_origins=["*"],
@@ -16,3 +66,6 @@ class Application:
             allow_methods=["*"],
             allow_headers=["*"],
         )
+
+        self.manager = ApplicationUsers()
+        self.history = ChatHistory()
--- ps-chat-fastapi/main.py
+++ ps-chat-fastapi/main.py
@@ -1,55 +1,47 @@
-from fastapi import WebSocket, WebSocketDisconnect
-
+from fastapi import Response, status
+from fastapi.responses import RedirectResponse
 from datetime import datetime
-import json
 
-from app import Application
+from app import Application, User, Message
 
 app = Application()
 
-@app.api.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await app.manager.connect(websocket)
-    now = datetime.now()
-    current_time = now.strftime("%H:%M")
-    try:
-        while True:
-            data = await websocket.receive_json() 
-            
-            if "login" in data and data["login"] not in app.manager.users.values():
-                app.manager.users[client_id] = data["login"]
-                message = {
-                    "time": current_time,
-                    "clientId": client_id,
-                    "message": "Online",
-                    "login": app.manager.users[client_id],
-                }
-                await app.manager.broadcast(json.dumps(message))
-                continue
-            
-            if "message" in data:
-            
-                # await app.manager.send_personal_message(f"You wrote: {data}", websocket)
-                message = {
-                    "time": current_time,
-                    "clientId": client_id,
-                    "message": data["message"], 
-                    "login": app.manager.users[client_id],
-                }
-                await app.manager.broadcast(json.dumps(message))
 
-    except WebSocketDisconnect:
-        app.manager.disconnect(websocket)
-        message = {
-            "time": current_time,
-            "clientId": client_id,
-            "message": "Offline",
-            "login": app.manager.users[client_id],
-        }
-        app.manager.users.pop(client_id)
-        await app.manager.broadcast(json.dumps(message))
+@app.api.get("/", status_code=308)
+async def root():
+    return RedirectResponse(url="/docs")
+
+
+@app.api.post("/login", status_code=200)
+async def login(body: User, response: Response):
+    if app.manager.add_user(body):
+        return {"response": "Connected"}
+    response.status_code = status.HTTP_400_BAD_REQUEST
+    return {"response": "Choose another login"}
+
+
+@app.api.get("/chat", status_code=200)
+async def chat_history():
+    return app.history.get_messages()
+
+
+@app.api.post("/chat", status_code=201)
+async def send(body: Message, response: Response):
+    body.time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    if app.history.add_message(body):
+        return {"response": "Delivered"}
+    response.status_code = status.HTTP_400_BAD_REQUEST
+    return {"response": "Not delivered"}
+
+
+@app.api.delete("/chat", status_code=201)
+async def delete(body: Message, response: Response):
+    raise NotImplementedError
+    pass
+
 
 # start
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app.api, host="0.0.0.0", port=8000)
