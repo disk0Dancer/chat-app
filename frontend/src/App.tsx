c18af89936dde000d9aae945a033b3bd60ba7352
disk0dancer <gachurakov@edu.hse.ru>
2024-12-10 12:34:32 +0500

    feat(ui): replace ps with ampq

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,111 +1,105 @@
-// @ts-expect-error - ignore TS error
 import React, { useState, useEffect } from 'react';
-import { PubSub } from '@google-cloud/pubsub';
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';import { Buffer } from 'buffer';
+window.Buffer = Buffer;
+// import { Client } from '@stomp/stompjs';
+
+import {ampq} from 'amqplib';
 
 type ChatMessage = {
     sender: string;
     message: string;
 };
 
-const SYSTEM_TOPIC = 'users';
-const CHAT_TOPIC = 'chat';
-export const pubSubClient = new PubSub({
-    projectId: 'chat',
-    apiEndpoint: '0.0.0.0:8085',
-});
+const SYSTEM_QUEUE = 'login';
+const CHAT_QUEUE = 'chat';
+
 
 function App() {
-    const [login, setLogin] = useState('');
+    const [userLogin, setUserLogin] = useState('');
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-    const [userTopic, setUserTopic] = useState<string>('');
+    const [messages, setMessages] = useState<ChatMessage[]>([]);
+    // @ts-ignore
+    const [userQueue, setUserQueue] = useState<string>('');
+    const [channel, setChannel] = useState(null);
+    const [connection, setConnection] = useState(null);
+
+    useEffect(() => {
+
+        const connection = ampq.connect("amqp://localhost:5672"); // await
+        const channel= connection.createChannel();
+
+        setChannel(channel);
+        setConnection(connection);
 
 
-    async function publishMessage(topicNameOrId: string, data: string) {
-        const dataBuffer = Buffer.from(data);
-        const topic = pubSubClient.topic(topicNameOrId);
+        return () => {
+            if (isLoggedIn && connection) {
+                window.addEventListener('beforeunload', logout);
+                connection.on("message", (message) => (
+                    setMessages([message, ...messages])
+                ))
+                window.removeEventListener('beforeunload', logout);
+            }
+        };
+    }, [isLoggedIn]);
 
-        try {
-            const messageId = topic.publishMessage({data: dataBuffer});
-            console.log(`Message ${messageId} published.`);
-        } catch (error) {
-            console.error(
-                `Received error while publishing: ${(error as Error).message}`
-            );
-            process.exitCode = 1;
-        }
-    }
-
-    const logout = async () => {
-        try {
-            const command = {
-                action: 'logout',
-                login: login,
-            };
-            await publishMessage(SYSTEM_TOPIC, JSON.stringify({ json: command }));
-            return;
-        } catch (err) {
-            setErrorMessage('Ошибка выхода' + err);
+    const publishMessage = async (queueName: string, data: string) => {
+        if (channel) {
+            await channel.sendToQueue(queueName, Buffer.from(JSON.stringify(data)));
+        } else {
+            console.error('err publish');
         }
     };
 
-    const sendMessage = async () => {
+    const sendMessage = () => {
         if (!message) {
             return;
         }
-        try {
-            const chatMessage = {
-                login: login,
-                message: message,
-            };
-            await publishMessage(CHAT_TOPIC,  JSON.stringify({ json: chatMessage }));
-            setMessage('');
-        } catch (err) {
-            setErrorMessage('Ошибка отправки сообщения' + err);
-        }
-        return;
+        const chatMessage = {
+            sender: userLogin,
+            message: message,
+        };
+        publishMessage(CHAT_QUEUE, JSON.stringify(chatMessage))
+            .then(() =>  setMessage(''));
     };
 
-    const handleLogin = async () => {
-        try {
-            const command = {
-                action: 'login',
-                login: login,
-            };
-
-            await publishMessage(SYSTEM_TOPIC, JSON.stringify({ json: command }));
-            setUserTopic(`chat_user_${login}`);
-            setLogin(login);
-            setIsLoggedIn(true);
-
-            const subscription = pubSubClient.subscription(userTopic);
-            subscription.on('message', message => {
-                const chatMessage = JSON.parse(
-                    message.data.toString(),
-                ) as ChatMessage;
-                setMessages(prev => [...prev, chatMessage]);
-                message.ack();
-            });
-        } catch (err) {
-            setErrorMessage(err);
-        }
+    const login = () => {
+        const userQueue = `${uuidv4()}`;
+        const command = {
+            action: 'login',
+            login: userLogin,
+            queue: userQueue,
+        };
+        publishMessage(SYSTEM_QUEUE, JSON.stringify(command))
+            .then(
+                () => (
+                    setIsLoggedIn(true), setUserQueue(userQueue)
+                )
+            );
     };
 
-    const enterClick = (e, func) => {
+    const logout = async () => {
+        const command = {
+            action: 'logout',
+            login: userLogin,
+        };
+        publishMessage(SYSTEM_QUEUE, JSON.stringify(command))
+            .then(() => setIsLoggedIn(false));
+        await connection.close(); // await
+        await channel.close();
+    };
+
+    const enterClick = (e: React.KeyboardEvent, func: () => void) => {
         if (e.key === 'Enter') {
             func();
         }
     };
 
-    useEffect(() => {
-        if (isLoggedIn) {
-            window.addEventListener('beforeunload', logout);
-            return;
-        }
-    }, [isLoggedIn, logout]);
+    // useEffect(() => {
+    //
+    // }, [isLoggedIn]);
 
     return (
         <div className="container">
@@ -116,66 +110,52 @@ function App() {
                         className="input-chat"
                         type="text"
                         placeholder="Login"
-                        value={login}
-                        onChange={e => setLogin(e.target.value)}
-                        onKeyDown={e => enterClick(e, handleLogin)}
+                        value={userLogin}
+                        onChange={(e) => setUserLogin(e.target.value)}
+                        onKeyDown={(e) => enterClick(e, login)}
                     />
-                    <button onClick={handleLogin} className="submit-chat">
+                    <button onClick={login} className="submit-chat">
                         Login
                     </button>
                 </div>
             ) : (
-                <div className={'container'}>
+                <div className="container">
                     <div className="chat-container">
                         <h1>Chat</h1>
-                        <h2>Your login: {login} </h2>
-                        {errorMessage && (
-                            <p className="error-message">{errorMessage}</p>
-                        )}
+                        <h2>Your login: {userLogin}</h2>
                         <div className="chat">
-                            {messages.map((value, index) => {
-                                // @ts-ignore
-                                if (value.user.login === login) {
-                                    return (
-                                        <div
-                                            key={index}
-                                            className="my-message-container"
-                                        >
-                                            <div className="my-message">
-                                                <p className="message">
-                                                    {value.message}
-                                                </p>
-                                            </div>
-                                        </div>
-                                    );
-                                } else {
-                                    return (
-                                        <div
-                                            key={index}
-                                            className="another-message-container"
-                                        >
-                                            <div className="another-message">
-                                                <p className="client">
-                                                    {value.login}
-                                                </p>
-                                                <p className="message">
-                                                    {value.message}
-                                                </p>
-                                            </div>
-                                        </div>
-                                    );
-                                }
-                            })}
+                            {messages.map((value, index) => (
+                                <div
+                                    key={index}
+                                    className={
+                                        value.sender === userLogin
+                                            ? 'my-message-container'
+                                            : 'another-message-container'
+                                    }
+                                >
+                                    <div
+                                        className={
+                                            value.sender === userLogin
+                                                ? 'my-message'
+                                                : 'another-message'
+                                        }
+                                    >
+                                        <p className="message">
+                                            {value.message}
+                                        </p>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                         <div className="input-chat-container">
                             <input
                                 className="input-chat"
                                 type="text"
                                 placeholder="Chat message ..."
-                                onChange={e => setMessage(e.target.value)}
-                                onKeyDown={e => enterClick(e, sendMessage)}
+                                onChange={(e) => setMessage(e.target.value)}
+                                onKeyDown={(e) => enterClick(e, sendMessage)}
                                 value={message}
-                            ></input>
+                            />
                             <button
                                 className="submit-chat"
                                 onClick={sendMessage}
@@ -185,10 +165,9 @@ function App() {
                         </div>
                     </div>
                 </div>
-            )
-            }
+            )}
         </div>
-    )
+    );
 }
 
 export default App;
